4c43b90a195153c6bda8af5a31da3369eb89b25a
romeucamurca <romeucamurcarabelo@gmail.com>
2021-08-27 20:03:30 -0300

    add script helps results

---
+++ RESULTS/memory/helps-to-consolidate-results.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd results-$1/
+
+cat file-6/avg-$1-file-6-$2-$3-$4.txt
+cat file-12/avg-$1-file-12-$2-$3-$4.txt
+cat file-24/avg-$1-file-24-$2-$3-$4.txt
+cat file-48/avg-$1-file-48-$2-$3-$4.txt
+cat file-96/avg-$1-file-96-$2-$3-$4.txt
+cat file-192/avg-$1-file-192-$2-$3-$4.txt
+cat file-384/avg-$1-file-384-$2-$3-$4.txt
+cat file-768/avg-$1-file-768-$2-$3-$4.txt
+cat file-1536/avg-$1-file-1536-$2-$3-$4.txt
+cat file-3072/avg-$1-file-3072-$2-$3-$4.txt
+cat file-6144/avg-$1-file-6144-$2-$3-$4.txt
+cat file-12288/avg-$1-file-12288-$2-$3-$4.txt
+cat file-24576/avg-$1-file-24576-$2-$3-$4.txt
+cat file-49152/avg-$1-file-49152-$2-$3-$4.txt
+cat file-98304/avg-$1-file-98304-$2-$3-$4.txt
+cat file-196608/avg-$1-file-196608-$2-$3-$4.txt
+cat file-393216/avg-$1-file-393216-$2-$3-$4.txt
+cat file-786432/avg-$1-file-786432-$2-$3-$4.txt
+cat file-1572864/avg-$1-file-1572864-$2-$3-$4.txt
