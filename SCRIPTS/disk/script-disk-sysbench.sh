0b923948a826d2bd6ed3dfd78bfe2d5aaeeabdc8
romeucamurca <romeucamurcarabelo@gmail.com>
2021-08-28 03:20:58 +0000

    add files of results sysbench e2-standard-4

--- SCRIPTS/disk/script-disk-sysbench.sh
+++ SCRIPTS/disk/script-disk-sysbench.sh
@@ -4,14 +4,14 @@ sudo apt update
 
 sudo apt install sysbench -y
 
-sysbench fileio --file-total-size=16G prepare
+sysbench fileio --file-total-size=32G prepare
 
 CONTADOR=1
 while [  $CONTADOR -lt 31 ]; do
-	echo "------------------------------------ÍNICIO DA EXECUÇÃO $CONTADOR------------------------------------------" >> results-test-disk-sysbench-e2-standard-2.txt
-        sysbench fileio --file-total-size=16G --time=120 --file-test-mode=rndrw --threads=2 run >> results-test-disk-sysbench-e2-standard-2.txt
-	echo "-------------------------------------FIM DA EXECUÇÃO $CONTADOR--------------------------------------------" >> results-test-disk-sysbench-e2-standard-2.txt
+	echo "------------------------------------ÍNICIO DA EXECUÇÃO $CONTADOR------------------------------------------" >> results-test-disk-sysbench-e2-standard-4.txt
+        sysbench fileio --file-total-size=32G --time=120 --file-test-mode=rndrw --threads=2 run >> results-test-disk-sysbench-e2-standard-4.txt
+	echo "-------------------------------------FIM DA EXECUÇÃO $CONTADOR--------------------------------------------" >> results-test-disk-sysbench-e2-standard-4.txt
         let CONTADOR=CONTADOR+1;
 done
 
-sysbench fileio --file-total-size=16G cleanup
+sysbench fileio --file-total-size=32G cleanup
