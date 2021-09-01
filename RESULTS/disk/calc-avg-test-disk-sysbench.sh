02b32e611a27cf3b819d7f6503f1b47f93e60639
romeucamurca <romeucamurcarabelo@gmail.com>
2021-08-31 21:12:46 -0300

    updating previous changes

--- RESULTS/disk/calc-avg-test-disk-sysbench.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cat $1 | grep -e '    read, MiB/s:' >> result-format-sysbench-$2.txt
-cat result-format-sysbench-$2.txt | cut -d ':' -f 2 | sed 's/ //g' | paste -s -d + | bc > total-sysbench-$2.txt
-echo "/30" >> total-sysbench-$2.txt
-cat total-sysbench-$2.txt | paste -s -d x | sed 's/x//' | bc >> media-sysbench-$2.txt
-sed -i '2d' total-sysbench-$2.txt
