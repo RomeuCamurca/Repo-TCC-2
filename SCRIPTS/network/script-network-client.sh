52e5a4c74348cc9b18894df12b1f712454427eb7
romeucamurca <romeucamurcarabelo@gmail.com>
2021-09-03 19:29:49 +0000

    add files of test network of e2-standard-2 client

---
+++ SCRIPTS/network/script-network-client.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install iperf3 -y
+
+CONTADOR=1
+while [  $CONTADOR -lt 31 ]; do
+	echo "------------------------------------ÍNICIO DA EXECUÇÃO $CONTADOR------------------------------------------" >> log-network-e2-standard-2.txt
+	iperf3 -t 120 -f M -i 5 -c 10.150.0.25 >> log-network-e2-standard-2.txt
+	echo "-------------------------------------FIM DA EXECUÇÃO $CONTADOR--------------------------------------------" >> log-network-e2-standard-2.txt
+        let CONTADOR=CONTADOR+1;
+done
