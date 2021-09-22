0eb487570d57450698f82387da4e2cda612a5c52
romeucamurca <romeucamurcarabelo@gmail.com>
2021-09-22 21:04:06 +0000

    add files of other test network of e2-standard-4 client

--- OTHER-EXPERIMENTS/SCRIPTS/network/script-network-client.sh
+++ OTHER-EXPERIMENTS/SCRIPTS/network/script-network-client.sh
@@ -6,8 +6,8 @@ sudo apt install iperf3 -y
 
 CONTADOR=1
 while [  $CONTADOR -lt 31 ]; do
-	echo "------------------------------------ÍNICIO DA EXECUÇÃO $CONTADOR------------------------------------------" >> log-network-e2-standard-2.txt
-	iperf3 -t 120 -f M -i 5 -c 10.150.0.32 >> log-network-e2-standard-2.txt
-	echo "-------------------------------------FIM DA EXECUÇÃO $CONTADOR--------------------------------------------" >> log-network-e2-standard-2.txt
+	echo "------------------------------------ÍNICIO DA EXECUÇÃO $CONTADOR------------------------------------------" >> log-network-e2-standard-4.txt
+	iperf3 -t 120 -f M -i 5 -c 10.150.0.34 >> log-network-e2-standard-4.txt
+	echo "-------------------------------------FIM DA EXECUÇÃO $CONTADOR--------------------------------------------" >> log-network-e2-standard-4.txt
         let CONTADOR=CONTADOR+1;
 done
